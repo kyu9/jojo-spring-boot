115ef5cfd4f3d1e21998b3fa4313c5b1405f36f1
kyu9 <kyudo97@naver.com>
2021-01-03 00:01:03 +0900

    API TESTCODE

--- src/test/java/com/kyu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/kyu/book/springboot/web/HelloControllerTest.java
@@ -1,6 +1,5 @@
 package com.kyu.book.springboot.web;
 
-import com.kyu.book.springboot.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
