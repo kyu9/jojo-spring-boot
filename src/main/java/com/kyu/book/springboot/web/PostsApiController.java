5e4e00905f97f6409a3213e035b1acdfabab322c
kyu9 <kyudo97@naver.com>
2021-01-04 22:30:18 +0900

    Add JPA, H2(insert, update)

---
+++ src/main/java/com/kyu/book/springboot/web/PostsApiController.java
@@ -0,0 +1,29 @@
+package com.kyu.book.springboot.web;
+
+import com.kyu.book.springboot.service.posts.PostsService;
+import com.kyu.book.springboot.web.dto.PostsResponseDto;
+import com.kyu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.kyu.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable long id){
+        return postsService.findById(id);
+    }
+}
