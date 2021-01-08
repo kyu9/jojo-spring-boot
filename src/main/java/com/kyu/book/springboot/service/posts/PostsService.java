5ab8666316df7243ebcd5e27664192807660ec33
kyu9 <kyudo97@naver.com>
2021-01-08 22:46:56 +0900

    posts creat, read

--- src/main/java/com/kyu/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/kyu/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,17 @@ package com.kyu.book.springboot.service.posts;
 
 import com.kyu.book.springboot.domain.posts.Posts;
 import com.kyu.book.springboot.domain.posts.PostsRepository;
+import com.kyu.book.springboot.web.dto.PostsListResponseDto;
 import com.kyu.book.springboot.web.dto.PostsResponseDto;
 import com.kyu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.kyu.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +36,10 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
---
+++ src/main/java/com/kyu/book/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.kyu.book.springboot.web.dto;
+
+import com.kyu.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
